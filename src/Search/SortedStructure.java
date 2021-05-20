f06d090fd2e8404eb0826327d174c1b2c97dea19
AgustinGH <agustingonzaleshunzicker@gmail.com>
2021-05-20 20:30:43 -0300

    merge two ordered list

--- src/Search/SortedStructure.java
+++ src/Search/SortedStructure.java
@@ -1,5 +1,9 @@
 package Search;
 
+import Structures.ListNode;
+
+import java.util.LinkedList;
+
 public class SortedStructure {
 
     public SortedStructure() {}
@@ -26,6 +30,56 @@ public class SortedStructure {
         }
         //No sum of pairs where N1 + N2 = N3
         return false;
+    }
+
+    // O(n) Inefficient memory usage
+    public LinkedList<Integer> mergeSortedList(LinkedList<Integer> l1, LinkedList<Integer> l2) {
+
+        int i = 0;
+
+        while (!l2.isEmpty()) {
+
+            if (i == l1.size()) {
+                break;
+            }
+
+            if (l1.get(i) > l2.peekFirst()) {
+                l1.add(i,l2.poll());
+            }
+
+            i++;
+        }
+
+        if (l1.isEmpty()) return l2;
+        if (!l2.isEmpty()) l1.addAll(l2);
+
+        return l1;
+    }
+
+    // O(n) or better, more efficient than "mergeSortedList"
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode last = new ListNode();
+        ListNode result = new ListNode(-1,last);
+
+        while (l1 != null && l2 != null) {
+
+            if (l1.val < l2.val) {
+                last.next = l1;
+                last = l1;
+                l1 = l1.next;
+            }
+            else {
+                last.next = l2;
+                last = l2;
+                l2 = l2.next;
+            }
+        }
+
+        if (l1 != null) last.next = l1;
+        if (l2 != null) last.next = l2;
+
+        return  result.next.next;
     }
 }
 
--- src/Search/SortedStructureTest.java
+++ src/Search/SortedStructureTest.java
@@ -1,9 +1,16 @@
 package Search;
 
+
+import Structures.ListNode;
+
+import java.util.LinkedList;
+
 public class SortedStructureTest {
 
     public static void main(String[] args) {
-        sumOfPairs();
+        //sumOfPairs();
+        //mergeSortedList();
+        mergeTwoLists();
     }
 
     private static void sumOfPairs() {
@@ -12,5 +19,53 @@ public class SortedStructureTest {
         SortedStructure s = new SortedStructure();
         if (s.sumOfPairs(args, n)) System.out.println("Exist sum of pairs");
         else System.out.println("Not exist sum of pairs");
+    }
+
+    private static void mergeSortedList() {
+
+        LinkedList<Integer> l1 = new LinkedList<>();
+        LinkedList<Integer> l2 = new LinkedList<>();
+
+        for (int i = 0; i < 10000; i++) {
+            l1.add(i*2);
+            l2.add(i*3);
+        }
+        System.out.print("Sorted List 1: ");
+        //for (int i = 0; i < 100000; i++) {
+        //    System.out.print(l1.get(i) + " ");
+        //}
+
+        System.out.println();
+        System.out.print("Sorted List 2: ");
+        //for (int i = 0; i < 100000; i++) {
+        //    System.out.print(l2.get(i) + " ");
+        //}
+        System.out.println();
+        System.out.print("Result Sorted List: ");
+        long init = System.currentTimeMillis();
+        SortedStructure s = new SortedStructure();
+        l1 = s.mergeSortedList(l1,l2);
+        //while (!l1.isEmpty()) {
+        //    System.out.print(l1.poll() + " ");
+        //}
+        System.out.println("time: "+(System.currentTimeMillis() - init));
+    }
+
+    private static void mergeTwoLists() {
+        ListNode next1 = new ListNode();
+        ListNode next2 = new ListNode();
+        ListNode l1 = next1;
+        ListNode l2 = next2;
+        for (int i = 1; i < 100000; i++) {
+            next1.next = new ListNode(i*2);
+            next2.next = new ListNode(i*3);
+            next1 = next1.next;
+            next2 = next2.next;
+        }
+
+        long init = System.currentTimeMillis();
+        SortedStructure s = new SortedStructure();
+        ListNode r = s.mergeTwoLists(l1,l2);
+        System.out.println("time: "+(System.currentTimeMillis() - init));
     }
 }
